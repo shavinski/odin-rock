3f3f16f10e5f1dc8edb761054b3f5d337c71dc38
Jakob Shavinski <shavinski.jakob@gmail.com>
2022-08-19 13:58:01 -0700

    added a getPlayerChoice function, game function, and got rid of checkWinner function

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # odin-rock
-Project rock paper scissors 
+Project rock paper scissors, 2-3 weeks into the javascript portion of fundamentals.
+
+First day I spent trying to figure out the fucntion of getComputerChoice.
+Then spent the next few days pracitcing and making functions, if statements etc. because I was stuck on how to do a playRound funciton.
+
+On 8/18 spent half of the time trying to make my own function but was finding no success. I began to look up solutions online 
+and found a solution that made sense.  
+
--- index.js
+++ index.js
@@ -3,40 +3,24 @@ const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice () {
     const choice = options[Math.floor(Math.random() * options.length)];
-    console.log(choice)
-    return choice
+    console.log("Computer chooses" +" " + choice);
+    return choice;
 }
 
-function checkWinner(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        return "Tie";
-    } else if (
-        (playerSelection == "rock" && computerSelection == "scissors") || 
-        (playerSelection == "paper" && computerSelection == "rock") ||
-        (playerSelection == "scissors" && computerSelection == "paper")
-    ){
-        return "Player";
-    } else {
-        return "Computer";
+function getPlayerChoice (){
+    let validatedInput = false;
+    while(validatedInput == false){
+        const choice = prompt("Please type rock paper or scissors");
+        if (choice == null){
+            continue;
+        } 
+        const choiceInLower = choice.toLowerCase();
+        if (options.includes(choiceInLower)) {
+            validatedInput = true;
+            return choiceInLower;
+        }
     }
 }
-
-function playRound(playerSelection, computerSelection){
-    const result = checkWinner(playerSelection, computerSelection);
-    if (result == "Tie") {
-        return "It is a tie";
-    } else if (result == "Player") {
-        return "Player 1 wins, " + playerSelection + " beats " +  computerSelection +".";
-    } else {
-        return "Computer wins, " + computerSelection + " beats "  + playerSelection +".";
-    }
-}
-
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-
-/*
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection == computerSelection) {
@@ -52,5 +36,30 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-*/
+function game() {
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+    console.log("Welcome!");
+    console.log("________");
+    for (let i=0; i < 5; i++) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("__________");
+        if(playRound(playerSelection, computerSelection) == "Player 1 wins, " + playerSelection + " beats " +  computerSelection +"." ) {
+            scorePlayer++;
+        } else if (playRound(playerSelection, computerSelection) == "Computer wins, " + computerSelection + " beats "  + playerSelection +".") {
+            scoreComputer++;
+        }
+    }
+    console.log("Game Over");
+    if(scorePlayer > scoreComputer){
+        console.log("Player 1 was the winner");
+    } else if (scorePlayer < scoreComputer){
+        console.log("Computer was the winner");
+    } else {
+        console.log ("We have a tie");
+    }
+}
 
+game()
