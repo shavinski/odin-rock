b06095368c932fb7a002959d6d163bff6b0fd05b
Jakob Shavinski <shavinski.jakob@gmail.com>
2023-01-12 21:24:58 -0800

    added to readme

--- README.md
+++ README.md
@@ -4,7 +4,10 @@ Project rock paper scissors, 2-3 weeks into the javascript portion of fundamenta
 First day I spent trying to figure out the fucntion of getComputerChoice.
 Then spent the next few days pracitcing and making functions, if statements etc. because I was stuck on how to do a playRound funciton.
 
-On 8/18 spent half of the time trying to make my own function but was finding no success. I began to look up solutions online 
+On 8/18/22 spent half of the time trying to make my own function but was finding no success. I began to look up solutions online 
 and found a solution that made sense.  
 
-Finished on 8/19 added a game, and getPlayerChoice function and got rid of checkWinner function
+Finished on 8/19/22 added a game, and getPlayerChoice function and got rid of checkWinner function
+
+1/12/23
+Back working with the odin project after interviewing for bootcamps and getting into Rithm School. Relearned git commands in terminal, relearned html and css since I have not used in a long time. Added a css style sheet were I designed the website. Started working on re coding my javascript to work on DOM manipulation. Able to update player choice and computer choice mystery boxes with updated images. Next time will need to work on scoring and ending the game with a score of >5.
--- index.js
+++ index.js
@@ -3,26 +3,30 @@ const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice () {
     const choice = options[Math.floor(Math.random() * options.length)];
-    console.log("Computer chooses" +" " + choice);
+    
+    if(choice === 'rock') {
+        const cpuRock = document.getElementById('computer-mystery');
+        let image = cpuRock
+        cpuRock.src = 'images/rock-emoji.png'
+    } else if (choice === 'paper') {
+        const cpuPaper = document.getElementById('computer-mystery');
+        let image = cpuPaper
+        cpuPaper.src = 'images/paper.png'
+    } else {
+        const cpuScissors = document.getElementById('computer-mystery');
+        let image = cpuScissors
+        cpuScissors.src = 'images/scissor.png'
+    };
+    
     return choice;
 }
 
 function getPlayerChoice (){
-    let validatedInput = false;
-    while(validatedInput == false){
-        const choice = prompt("Please type rock paper or scissors");
-        if (choice == null){
-            continue;
-        } 
-        const choiceInLower = choice.toLowerCase();
-        if (options.includes(choiceInLower)) {
-            validatedInput = true;
-            return choiceInLower;
-        }
-    }
-}
+    let choice = document.qu
+};
 
 function playRound(playerSelection, computerSelection){
+
     if (playerSelection == computerSelection) {
         return "It is a tie";
     } else if (
@@ -33,33 +37,72 @@ function playRound(playerSelection, computerSelection){
         return "Player 1 wins, " + playerSelection + " beats " +  computerSelection +".";
     } else {
         return "Computer wins, " + computerSelection + " beats "  + playerSelection +".";
-    }
-}
+    };
+};
+
+
+const playerRock = document.querySelector('.rock-option')
+playerRock.addEventListener('click', () => {
+    let image = document.getElementById('player-mystery');
+    let getPlayerChoice = 'rock';
+    
+    image.src = 'images/rock-emoji.png'
+
+    getComputerChoice()
+    console.log('player:', getPlayerChoice, 'cpu:', getComputerChoice());
+    return getPlayerChoice
+});
+
+const playerPaper = document.querySelector('.paper-option')
+playerPaper.addEventListener('click', () => {
+    let image = document.getElementById('player-mystery');
+    let getPlayerChoice = 'paper';
+    
+    image.src = 'images/paper.png'
+
+    getComputerChoice()
+    console.log('player:', getPlayerChoice, 'cpu:', getComputerChoice());
+    return getPlayerChoice
+});
+
+const playerScissors = document.querySelector('.scissor-option')
+playerScissors.addEventListener('click', () => {
+    let image = document.getElementById('player-mystery');
+    let getPlayerChoice = 'scissors';
+    
+    image.src = 'images/scissor.png'
+
+    getComputerChoice()
+    console.log('player:', getPlayerChoice, 'cpu:', getComputerChoice());
+    return getPlayerChoice
+});
 
 function game() {
     let scorePlayer = 0;
     let scoreComputer = 0;
-    console.log("Welcome!");
-    console.log("________");
+
     for (let i=0; i < 5; i++) {
         const playerSelection = getPlayerChoice();
         const computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection));
         console.log("__________");
-        if(playRound(playerSelection, computerSelection) == "Player 1 wins, " + playerSelection + " beats " +  computerSelection +"." ) {
+
+        if  (playRound(playerSelection, computerSelection) == "Player 1 wins, " + playerSelection + " beats " +  computerSelection +"." ) {
             scorePlayer++;
         } else if (playRound(playerSelection, computerSelection) == "Computer wins, " + computerSelection + " beats "  + playerSelection +".") {
             scoreComputer++;
-        }
+        };
     }
+
     console.log("Game Over");
+
     if(scorePlayer > scoreComputer){
         console.log("Player 1 was the winner");
     } else if (scorePlayer < scoreComputer){
         console.log("Computer was the winner");
     } else {
         console.log ("We have a tie");
-    }
-}
+    };
+};
 
 // game()
